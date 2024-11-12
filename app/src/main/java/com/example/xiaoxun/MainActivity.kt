12ef9cbf17980056e173edbc074d8c6037563305
小寻（李俊然） <lijunran@xiaohongshu.com>
2024-11-12 14:02:54 +0800

    feat:调整代码

--- app/src/main/java/com/example/xiaoxun/MainActivity.kt
+++ app/src/main/java/com/example/xiaoxun/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.xiaoxun
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
