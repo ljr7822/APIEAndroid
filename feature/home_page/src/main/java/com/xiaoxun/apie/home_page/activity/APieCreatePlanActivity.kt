27871c9febbb48456253c3d261ef25f7c34e399f
小寻（李俊然） <lijunran@xiaohongshu.com>
2025-01-01 17:31:49 +0800

    feat:添加全局转场动画

--- feature/home_page/src/main/java/com/xiaoxun/apie/home_page/activity/APieCreatePlanActivity.kt
+++ feature/home_page/src/main/java/com/xiaoxun/apie/home_page/activity/APieCreatePlanActivity.kt
@@ -1,14 +1,15 @@
 package com.xiaoxun.apie.home_page.activity
 
+import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.xiaoxun.apie.common.HOME_CREATE_PLAN_ACTIVITY_PATH
 import com.xiaoxun.apie.common.base.activity.APieBaseBindingActivity
+import com.xiaoxun.apie.home_page.R
 import com.xiaoxun.apie.home_page.databinding.LayoutApieCreatePlanActivityBinding
 
 @Route(path = HOME_CREATE_PLAN_ACTIVITY_PATH)
-class APieCreatePlanActivity :
-    APieBaseBindingActivity<LayoutApieCreatePlanActivityBinding>(
-        LayoutApieCreatePlanActivityBinding::inflate
-    ) {
+class APieCreatePlanActivity : APieBaseBindingActivity<LayoutApieCreatePlanActivityBinding>(
+    LayoutApieCreatePlanActivityBinding::inflate
+) {
 
 }
