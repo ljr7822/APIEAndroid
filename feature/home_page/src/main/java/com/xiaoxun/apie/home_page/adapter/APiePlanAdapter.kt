0792febe2082a3be0edd32fdb95786c7cfba61e4
小寻（李俊然） <lijunran@xiaohongshu.com>
2025-01-06 11:27:49 +0800

    feat:添加任务类型标识

--- feature/home_page/src/main/java/com/xiaoxun/apie/home_page/adapter/APiePlanAdapter.kt
+++ feature/home_page/src/main/java/com/xiaoxun/apie/home_page/adapter/APiePlanAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.xiaoxun.apie.common.ui.GlideCircleImageView
 import com.xiaoxun.apie.common_model.home_page.plan.PlanModel
 import com.xiaoxun.apie.home_page.R
+import com.xiaoxun.apie.home_page.viewmodel.PlanListType
 
 class APiePlanAdapter(
     private val items: MutableList<PlanModel> = mutableListOf()
@@ -27,7 +28,7 @@ class APiePlanAdapter(
         val planGroupName: TextView = view.findViewById(R.id.planGroupName)
         val planFrequency: TextView = view.findViewById(R.id.planFrequency)
         val goldValue: TextView = view.findViewById(R.id.goldValue)
-        val groupIcon: GlideCircleImageView = view.findViewById(R.id.groupIcon)
+        val planTypeIcon: GlideCircleImageView = view.findViewById(R.id.planTypeIcon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,6 +45,30 @@ class APiePlanAdapter(
         holder.goldValue.text = item.planAward.toString()
         holder.planGroupName.text = item.planGroupModel.groupName
         holder.planFrequency.text = "每天${item.planFrequency}次"
-        holder.groupIcon.loadImage(item.planGroupModel.groupIcon)
+        val typeIcon = when(item.planType) {
+            PlanListType.SINGLE_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_single_type_icon
+            }
+            PlanListType.TODAY_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_today_type_icon
+            }
+            PlanListType.WEEK_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_week_type_icon
+            }
+            PlanListType.MONTH_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_month_type_icon
+            }
+            PlanListType.YEAR_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_yera_type_icon
+            }
+            PlanListType.CYCLE_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_cycle_type_icon
+            }
+            else -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_custom_type_icon
+            }
+        }
+
+        holder.planTypeIcon.loadImage(typeIcon)
     }
 }
--- feature/home_page/src/main/java/com/xiaoxun/apie/home_page/adapter/APiePlanFrequencyAdapter.kt
+++ feature/home_page/src/main/java/com/xiaoxun/apie/home_page/adapter/APiePlanFrequencyAdapter.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.MainThread
 import androidx.recyclerview.widget.RecyclerView
+import com.xiaoxun.apie.common.ui.GlideCircleImageView
 import com.xiaoxun.apie.common.utils.setDebouncingClickListener
+import com.xiaoxun.apie.common.utils.show
 import com.xiaoxun.apie.home_page.R
 import com.xiaoxun.apie.home_page.viewmodel.PlanListType
 
@@ -27,6 +29,8 @@ class APiePlanFrequencyAdapter(
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val planFrequencyItemLayout: View = view.findViewById(R.id.planFrequencyItemLayout)
+        val planTypeIcon: GlideCircleImageView = view.findViewById(R.id.planTypeIcon)
         val planFrequencyName: TextView = view.findViewById(R.id.planFrequencyName)
     }
 
@@ -42,19 +46,58 @@ class APiePlanFrequencyAdapter(
     override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         val item = items[position]
         holder.planFrequencyName.text = item.desc
-        if (position == selectedPosition) {
-            holder.planFrequencyName.setBackgroundResource(R.drawable.apie_frequency_plan_selected_bg)
-            holder.planFrequencyName.setTextColor(holder.itemView.context.getColor(com.xiaoxun.apie.common.R.color.apie_color_6F94F4))
-            holder.planFrequencyName.setTypeface(null, Typeface.BOLD)
-        } else {
-            holder.planFrequencyName.setBackgroundResource(R.drawable.apie_frequenct_plan_normal_bg)
-            holder.planFrequencyName.setTextColor(holder.itemView.context.getColor(com.xiaoxun.apie.common.R.color.apieTheme_colorBlack_alpha_40))
-            holder.planFrequencyName.setTypeface(null, Typeface.NORMAL) // 正常字体
-        }
+        setSelectedPosition(holder, position)
         holder.planFrequencyName.setDebouncingClickListener {
             updateSelectedPosition(position)
             itemClick(position, item)
         }
+        bindPlanTypeIcon(holder, item)
+    }
+
+    private fun setSelectedPosition(holder: ViewHolder, position: Int) {
+        if (position == selectedPosition) {
+            holder.planFrequencyItemLayout.setBackgroundResource(R.drawable.apie_frequency_plan_selected_bg)
+            holder.planFrequencyName.setTextColor(holder.itemView.context.getColor(com.xiaoxun.apie.common.R.color.apie_color_6F94F4))
+            holder.planFrequencyName.setTypeface(null, Typeface.BOLD)
+        } else {
+            holder.planFrequencyItemLayout.setBackgroundResource(R.drawable.apie_frequenct_plan_normal_bg)
+            holder.planFrequencyName.setTextColor(holder.itemView.context.getColor(com.xiaoxun.apie.common.R.color.apieTheme_colorBlack_alpha_40))
+            holder.planFrequencyName.setTypeface(null, Typeface.NORMAL) // 正常字体
+        }
+    }
+
+    private fun bindPlanTypeIcon(holder: ViewHolder, item: PlanListType) {
+        holder.planTypeIcon.show()
+        val typeIcon = when (item.type) {
+            PlanListType.SINGLE_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_single_type_icon
+            }
+
+            PlanListType.TODAY_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_today_type_icon
+            }
+
+            PlanListType.WEEK_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_week_type_icon
+            }
+
+            PlanListType.MONTH_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_month_type_icon
+            }
+
+            PlanListType.YEAR_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_yera_type_icon
+            }
+
+            PlanListType.CYCLE_PLAN.type -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_cycle_type_icon
+            }
+
+            else -> {
+                com.xiaoxun.apie.common.R.drawable.apie_plan_custom_type_icon
+            }
+        }
+        holder.planTypeIcon.loadImage(typeIcon, 10)
     }
 
     fun updateSelectedPosition(position: Int) {
